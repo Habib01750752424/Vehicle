5a746f88039fd8b67346a7d9b1c2285a5af9d57b
Md. Habibur Rahman <habibcsepust@gmail.com>
2019-10-15 23:00:36 +0600

    Added

---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    
+    public partial class VehicleUi : Form
+    {
+        Vehicle vehicle = new Vehicle();
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            vehicle.Set(vehicleNameTextBox.Text,regNoTextBox.Text);
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            vehicle.SetSpeed(Convert.ToDouble(speedTextBox.Text));
+            speedTextBox.Clear();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(vehicle.GetMax() + " " + vehicle.GetMin() + " " + vehicle.GetAvg());
+            minSpeedTextBox.Text = vehicle.GetMin().ToString();
+            maxSpeedTextBox.Text = vehicle.GetMax().ToString();
+            averageSpeedTextBox.Text = vehicle.GetAvg().ToString();
+        }
+    }
+}
